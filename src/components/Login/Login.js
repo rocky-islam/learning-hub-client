e7532afa2bc9fa36a6e9edee2d6368457b598c30
rocky-islam <62227570+rocky-islam@users.noreply.github.com>
2022-10-27 12:20:06 +0600

    all navigate done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -44,6 +44,7 @@ const Login = () => {
       .then((result) => {
         const user = result.user;
         console.log(user);
+        navigate(from, { replace: true });
       })
       .catch((error) => console.error(error));
   };
@@ -55,6 +56,7 @@ const Login = () => {
         .then((result) => {
           const user = result.user;
           console.log(user);
+          navigate(from, { replace: true });
         })
         .catch((error) => console.error(error));
   }
